82730a4e7f77914fb8535e1624a7cd0b91122815
Vincent Liew <98124240+vjliew@users.noreply.github.com>
2023-10-25 13:57:12 -0500

    File functions and utils file

--- comm-test/Server.py
+++ comm-test/Server.py
@@ -1,50 +1,18 @@
-import socket
-import threading
 import paramiko
+from paramiko import SSHClient
+from paramiko import Transport
+from scp import SCPClient
 
+# Create an SSH client
+ssh = SSHClient()
+ssh.load_system_host_keys()
+ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
-class SSHServer(paramiko.ServerInterface):
-    def __init__(self):
-        self.event = threading.Event()
+# Connect to the SSH server
+ssh.connect('127.0.0.1', port=8888, username='username', password='password')
 
-    def check_channel_request(self, kind, chanid):
-        if kind == 'session':
-            return paramiko.OPEN_SUCCEEDED
-        return paramiko.OPEN_FAILED_ADMINISTRATIVELY_PROHIBITED
+# Send a file to the server
+send_file(ssh, 'path_to_your_file.txt')
 
-    def check_auth_password(self, username, password):
-        if username == 'username' and password == 'password':
-            return paramiko.AUTH_SUCCESSFUL
-        return paramiko.AUTH_FAILED
-
-
-server_host = '127.0.0.1'
-server_port = 8888
-
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-server_socket.bind((server_host, server_port))
-
-server_socket.listen()
-
-print(f"Server is listening on {server_host}:{server_port}")
-
-while True:
-    # Accept a client connection
-    client_socket, client_address = server_socket.accept()
-
-    transport = paramiko.Transport(client_socket)
-    transport.add_server_key(paramiko.RSAKey.generate(2048))
-    server = SSHServer()
-
-    transport.start_server(server=server)
-
-    channel = transport.accept()
-    if channel is None:
-        print("SSH negotiation failed.")
-        client_socket.close()
-        continue
-
-    print("SSH connection established.")
-
-    channel.send("Welcome to the custom SSH server!")
+# Close the SSH connection
+ssh.close()
---
+++ comm-test/utils.py
@@ -0,0 +1,59 @@
+import paramiko
+from paramiko import SSHClient
+from paramiko import Transport
+from scp import SCPClient
+import zipfile
+import os
+
+def zip_file(file, zip_file_name):
+    try:
+        with zipfile.ZipFile(zip_file_name, 'w', zipfile.ZIP_DEFLATED) as zipf:
+            zipf.write(file, arcname=file)
+        print(f'{file} has been zipped to {zip_file_name}')
+    except Exception as e:
+        print(f'Error zipping file: {e}')
+
+def unzip_file(zip_file, destination_folder):
+    try:
+        with zipfile.ZipFile(zip_file, 'r') as zipf:
+            zipf.extractall(destination_folder)
+        print(f'{zip_file} has been successfully unzipped to {destination_folder}')
+    except Exception as e:
+        print(f'Error unzipping file: {e}')
+      
+
+def send_scp_file(client, channel, local_file, remote_path, file_name, file_size):
+    zip_file(file_name, 'something.zip')
+    received = False
+
+    while not received:
+        try:
+            with SCPClient(client.get_transport()) as scp:
+                scp.put(local_file, remote_path)
+                print(f"Sent file: {local_file}")
+        except Exception as e:
+            print(f"Error sending file: {e}")
+
+        msg = file_name + ';' + file_size
+        send_message(channel, msg)
+
+        msg = receive_message(channel)
+        if msg == 'Received':
+            received = True
+
+    
+def receive_scp_file(channel, destination_file_path):
+    received = False
+    while not received:
+        msg = receive_message(channel)
+        split_msg = msg.split(';')
+        if os.path.exists(split_msg[0]):
+            unzip_file(split_msg[0], destination_file_path)
+
+            # check file size equal
+            file_size = os.path.getsize(destination_file_path)
+            if file_size == int(split_msg[1]):
+                send_message(channel, "Received")
+                received = True
+            else:
+                send_message(channel, "Resend")
