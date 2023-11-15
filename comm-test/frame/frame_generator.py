92c76cd79429aec30d2a61bda039bf70da6cfe29
Vincent Liew <98124240+vjliew@users.noreply.github.com>
2023-11-15 12:07:00 -0600

    Use Dataloader to pass through model

--- comm-test/frame/frame_generator.py
+++ comm-test/frame/frame_generator.py
@@ -3,14 +3,95 @@ import matplotlib.pyplot as plt
 from matplotlib import image
 import os
 import cv2
-from frame_helpers import colorize, convert_to_bgra_if_required
-from pyk4a import PyK4APlayback
+# from frame_helpers import colorize, convert_to_bgra_if_required
+# from pyk4a import PyK4APlayback
 import h5py
 from torchvision import transforms
 from torch.utils.data import DataLoader, Dataset
 import random
 import torch
 import argparse
+# from unet.model import UNet
+
+import torch
+from torch import nn
+
+class DoubleConv(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(DoubleConv, self).__init__()
+        self.double_conv = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True),
+        )
+
+    def forward(self, x):
+        return self.double_conv(x)
+
+
+class DownBlock(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(DownBlock, self).__init__()
+        self.double_conv = DoubleConv(in_channels, out_channels)
+        self.down_sample = nn.MaxPool2d(2)
+
+    def forward(self, x):
+        skip_out = self.double_conv(x)
+        down_out = self.down_sample(skip_out)
+        return (down_out, skip_out)
+
+
+class UpBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, up_sample_mode):
+        super(UpBlock, self).__init__()
+        if up_sample_mode == 'conv_transpose':
+            self.up_sample = nn.ConvTranspose2d(in_channels-out_channels, in_channels-out_channels, kernel_size=2, stride=2)
+        elif up_sample_mode == 'bilinear':
+            self.up_sample = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
+        else:
+            raise ValueError("Unsupported `up_sample_mode` (can take one of `conv_transpose` or `bilinear`)")
+        self.double_conv = DoubleConv(in_channels, out_channels)
+
+    def forward(self, down_input, skip_input):
+        x = self.up_sample(down_input)
+        x = torch.cat([x, skip_input], dim=1)
+        return self.double_conv(x)
+
+
+class UNet(nn.Module):
+    def __init__(self, out_classes=1, up_sample_mode='conv_transpose'):
+        super(UNet, self).__init__()
+        self.up_sample_mode = up_sample_mode
+        # Downsampling Path
+        self.down_conv1 = DownBlock(3, 64)
+        self.down_conv2 = DownBlock(64, 128)
+        self.down_conv3 = DownBlock(128, 256)
+        self.down_conv4 = DownBlock(256, 512)
+        # Bottleneck
+        self.double_conv = DoubleConv(512, 1024)
+        # Upsampling Path
+        self.up_conv4 = UpBlock(512 + 1024, 512, self.up_sample_mode)
+        self.up_conv3 = UpBlock(256 + 512, 256, self.up_sample_mode)
+        self.up_conv2 = UpBlock(128 + 256, 128, self.up_sample_mode)
+        self.up_conv1 = UpBlock(128 + 64, 64, self.up_sample_mode)
+        # Final Convolution
+        self.conv_last = nn.Conv2d(64, out_classes, kernel_size=1)
+
+    def forward(self, x):
+        x, skip1_out = self.down_conv1(x)
+        x, skip2_out = self.down_conv2(x)
+        x, skip3_out = self.down_conv3(x)
+        x, skip4_out = self.down_conv4(x)
+        x = self.double_conv(x)
+        x = self.up_conv4(x, skip4_out)
+        x = self.up_conv3(x, skip3_out)
+        x = self.up_conv2(x, skip2_out)
+        x = self.up_conv1(x, skip1_out)
+        x = self.conv_last(x)
+        return x
 
 class CustomDataset(Dataset):
     """
@@ -70,63 +151,63 @@ class CustomDataset(Dataset):
             
         return img, truth
 
-def info(playback: PyK4APlayback):
-    print(f"Record length: {playback.length / 1000000: 0.2f} sec")
+# def info(playback: PyK4APlayback):
+#     print(f"Record length: {playback.length / 1000000: 0.2f} sec")
           
 # Load an MKV file
-def create_frames(path: str):
-    # Load an MKV file
-    if os.path.exists(f"{path}"):
-        mkv_file = f"{path}"
-    else:
-        print("That path doesn't exist bruh lol")
-        exit()
+# def create_frames(path: str):
+#     # Load an MKV file
+#     if os.path.exists(f"{path}"):
+#         mkv_file = f"{path}"
+#     else:
+#         print("That path doesn't exist bruh lol")
+#         exit()
 
-    playback = PyK4APlayback(mkv_file)
-    playback.open()
-    playback.seek(5000000) # in microseconds, aka 5 seconds
-    info(playback) # prints the recording length
+#     playback = PyK4APlayback(mkv_file)
+#     playback.open()
+#     playback.seek(5000000) # in microseconds, aka 5 seconds
+#     info(playback) # prints the recording length
 
-    # print(type(playback))
-    # Create a new HDF5 file
-    file = h5py.File('output_1.h5', 'w')
+#     # print(type(playback))
+#     # Create a new HDF5 file
+#     file = h5py.File('output_1.h5', 'w')
 
-    # Create datasets for RGB images and depth maps
-    rgb_images = file.create_dataset('images', (0, 640, 480, 3), maxshape=(None, 640,480, 3))
-    depth_images = file.create_dataset('depths', (0, 640, 480), maxshape=(None, 640, 480))
+#     # Create datasets for RGB images and depth maps
+#     rgb_images = file.create_dataset('images', (0, 640, 480, 3), maxshape=(None, 640,480, 3))
+#     depth_images = file.create_dataset('depths', (0, 640, 480), maxshape=(None, 640, 480))
 
-    i = 0
-    while True:
-        try:
-            capture = playback.get_next_capture()
+#     i = 0
+#     while True:
+#         try:
+#             capture = playback.get_next_capture()
 
-            if capture.color is not None:
-                img_color = cv2.resize(cv2.cvtColor(convert_to_bgra_if_required(0, capture.color), cv2.COLOR_BGR2RGB)[80:720, 446: 926, 0:3], (480, 640))
+#             if capture.color is not None:
+#                 img_color = cv2.resize(cv2.cvtColor(convert_to_bgra_if_required(0, capture.color), cv2.COLOR_BGR2RGB)[80:720, 446: 926, 0:3], (480, 640))
 
-                # Append the RGB image to the dataset
-                rgb_images.resize(i + 1, axis=0)
-                rgb_images[i] = img_color
+#                 # Append the RGB image to the dataset
+#                 rgb_images.resize(i + 1, axis=0)
+#                 rgb_images[i] = img_color
 
-            if capture.depth is not None:
-                img_depth = cv2.resize(capture.transformed_depth[80:720, 446: 926], (480, 640))
+#             if capture.depth is not None:
+#                 img_depth = cv2.resize(capture.transformed_depth[80:720, 446: 926], (480, 640))
 
-                # Append the depth map to the dataset
-                depth_images.resize(i + 1, axis=0)
-                depth_images[i] = img_depth
+#                 # Append the depth map to the dataset
+#                 depth_images.resize(i + 1, axis=0)
+#                 depth_images[i] = img_depth
 
-            i += 1
+#             i += 1
 
-            # key = cv2.waitKey(0)
-            # if key != -1:
-            #     break
-        except EOFError:
-            break
+#             # key = cv2.waitKey(0)
+#             # if key != -1:
+#             #     break
+#         except EOFError:
+#             break
 
-    # Close the HDF5 file
-    file.close()
+#     # Close the HDF5 file
+#     file.close()
 
-    # Close the MKV file
-    playback.close()
+#     # Close the MKV file
+#     playback.close()
 
 def create_data_set(file_name):
     mat_file = h5py.File(f'{file_name}', 'r')
@@ -153,14 +234,35 @@ def create_data_set(file_name):
 
 def create_data_loader(x, y):
     dataset = CustomDataset(x, y)
-    loader = DataLoader(dataset, batch_size=4, shuffle=True)
+    loader = DataLoader(dataset, batch_size=1, shuffle=True)
     return loader
+    
 
 def main(path: str):
-    create_frames(f"{path}")
+    # create_frames(f"{path}")
     x, y = create_data_set("output_1.h5")
     loader = create_data_loader(x, y)
     print("done")
+    device = (
+        "cuda"
+        if torch.cuda.is_available()
+        else "mps"
+        if torch.backends.mps.is_available()
+        else "cpu"
+    )
+    print(f"Now using device: {device}")
+    model = UNet()
+    model = UNet().to(torch.device(device))
+    model.eval()
+    print(f"Put model on device")
+    with torch.no_grad():
+        for batch_idx, (image, truth) in enumerate(loader):
+            image = image.to(torch.device(device))
+            truth = truth.to(torch.device(device))
+            
+            outputs = model(image)
+            print(f"Model ran for batch {batch_idx}")
+
 
 if __name__ == "__main__":
     argsparser = argparse.ArgumentParser()
